434fa37bc72b0ada944d621776ae2318ab406d8d
Mindaugas Milius <mindaugas.milius@stud.hs-hannover.de>
2016-11-08 18:26:42 +0100

    fixing comit errors

--- src/FootballData.php
+++ src/FootballData.php
@@ -1,8 +1,6 @@
 <?php
 namespace Grambas\FootballData;
-
 use GuzzleHttp\Client;
-
 class FootballData
 {
 	protected $client;
@@ -11,7 +9,6 @@ class FootballData
 	{	
 		$this->client = $client;
 	}
-
 	//COMPETITION/LEAGUE
 	public function LeagueTable($id, $matchday="")
 	 	{
@@ -23,26 +20,22 @@ class FootballData
 		$leagues = $this->client->get("competitions/?season={$year}")->getBody();
 		return json_decode($leagues);
 	}
-
 	public function  getLeagueTeams($id)
 	 	{
 		$leagueTeams = $this->client->get("competitions/{$id}/teams")->getBody();
 		return json_decode($leagueTeams);
 	}
-
 	public function getLeagueFixtures($id, $matchday="", $timeFrame="")
 	 	{
 		$leagueFixtures = $this->client->get("competitions/{$id}/fixtures/?matchday={$matchday}&timeFrame={$timeFrame}")->getBody();
 		return json_decode($leagueFixtures);
 	}
-
 	//FIXTURES
 	public function getFixture($id, $head="")
 	{	
 		$fixture = $this->client->get( "fixtures/{$id}/?head2head={$head}")->getBody();
 		return json_decode($fixture);
 	}
-
 	public function getFixturesOfSet($leagueCode="", $timeFrame="")
 	{	
 		$fixtures = $this->client->get( "fixtures/?leagueCode={$leagueCode}&timeFrame={$timeFrame}")->getBody();
@@ -53,14 +46,12 @@ class FootballData
 		$teamFixtures = $this->client->get("teams/{$id}/fixtures/?season={$season}&timeFrame={$timeFrame}&venue={$venue}")->getBody();
 		return json_decode($teamFixtures);
 	}
-
 	// TEAM
 	public function getTeam($id)
 	{
 		$team = $this->client->get("teams/{$id}")->getBody();
 		return json_decode($team);		
 	}
-
 	//PLAYERS
 	public function getTeamPlayers($id)
 	{
